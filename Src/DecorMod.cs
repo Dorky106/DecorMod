c1d659dedfa577ad1ca5b3c70db51f47eda68db3
JackPS9 <greasontim@gmail.com>
2018-06-09 19:24:45 -0400

    Init

---
+++ Src/DecorMod.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace DecorMod
+{
+	[ModLoader.ModManager]
+	public static class DecorMod
+	{
+		public static string ModFolder;
+		[ModLoader.ModCallback(ModLoader.EModCallbackType.OnAssemblyLoaded, "DecorMod.OnAssemblyLoaded")]
+		public static void OnAssemblyLoaded(string path)
+		{
+			ModFolder = Path.GetDirectoryName(path);
+		}
+
+		[ModLoader.ModCallback(ModLoader.EModCallbackType.AfterStartup, "DecorMod.AfterStartup")]
+		[ModLoader.ModCallbackProvidesFor("phentrixgames.newcolonyapi.AfterStartup")]
+		public static void AfterStartup()
+		{
+			PhentrixGames.NewColonyAPI.Managers.ModManager.RegisterMod("DecorMod", ModFolder);
+		}
+	}
+}
